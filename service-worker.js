8cf331ae1d094c465d61adb537db6b148344c692
Andre do Nascimento Gomes <anascimento@local.lifting.com.br>
2026-06-10 09:57:20 -0300

    fix: atualiza versao do cache do service worker para forcar reload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='dnd-v30-cloud-v1';
+const CACHE='dnd-v31-auth-v1';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
